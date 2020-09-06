8b8e7ffb3c196a0ca8ca517be3d520bf57a66786
Ricardo Note <shyoutarou@gmail.com>
2020-09-06 17:25:04 -0300

    Add MainViewModel

---
+++ LoveTrips.Core/Interfaces/ViewModels/IMainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoveTrips.Core.Interfaces.ViewModels
+{
+    public interface IMainViewModel
+    {
+    }
+}
---
+++ LoveTrips.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using LoveTrips.Core.Interfaces.ViewModels;
+using MvvmCross;
+using MvvmCross.ViewModels;
+using System;
+
+namespace LoveTrips.Core.ViewModels
+{
+    public class MainViewModel : MvxViewModel, IMainViewModel
+    {
+        private readonly Lazy<TipViewModel> _tipViewModel;
+
+        public TipViewModel TipViewModel => _tipViewModel.Value;
+
+        public MainViewModel()
+        {
+            _tipViewModel = new Lazy<TipViewModel>(Mvx.IoCProvider.IoCConstruct<TipViewModel>);
+        }
+    }
+}
