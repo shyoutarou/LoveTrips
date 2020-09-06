af5fdfc2ea350eaee04df1e6a7b66a340a80a416
Ricardo Note <shyoutarou@gmail.com>
2020-09-06 17:25:33 -0300

    Update App and Add AppStart

--- LoveTrips.Core/App.cs
+++ LoveTrips.Core/App.cs
@@ -2,6 +2,7 @@
 using MvvmCross.ViewModels;
 using LoveTrips.Core.Services;
 using LoveTrips.Core.ViewModels;
+using MvvmCross.IoC;
 
 namespace LoveTrips.Core
 {
@@ -10,7 +11,31 @@ namespace LoveTrips.Core
         public override void Initialize()
         {
             Mvx.IoCProvider.RegisterType<ICalculationService, CalculationService>();
-            RegisterAppStart<TipViewModel>();
+
+            CreatableTypes()
+                .EndingWith("Repository")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            //// Construct custom application start object
+            //Mvx.ConstructAndRegisterSingleton<IMvxAppStart, AppStart>();
+
+            //// request a reference to the constructed appstart object 
+            //var appStart = Mvx.IoCProvider.Resolve<IMvxAppStart>();
+
+            //// register the appstart object
+            //RegisterAppStart(appStart);
+
+            //RegisterAppStart<TipViewModel>();
+
+
+            // register the appstart object
+            RegisterCustomAppStart<AppStart>();
         }
     }
 }
---
+++ LoveTrips.Core/AppStart.cs
@@ -0,0 +1,32 @@
+﻿using LoveTrips.Core.Interfaces.Services;
+using LoveTrips.Core.ViewModels;
+using MvvmCross;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+using System.Threading.Tasks;
+
+namespace LoveTrips.Core
+{
+    public class AppStart : MvxAppStart
+    {
+        private readonly ILoginService _loginService;
+
+        public AppStart(IMvxApplication app, IMvxNavigationService mvxNavigationService)
+                : base(app, mvxNavigationService)
+        {
+            _loginService = Mvx.IoCProvider.Resolve<ILoginService>();
+        }
+
+        protected override Task NavigateToFirstViewModel(object hint = null)
+        {
+            if (_loginService.Login("shyo", "123"))
+            {
+                return NavigationService.Navigate<TipViewModel>();
+            }
+            else
+            {
+                return NavigationService.Navigate<LoginViewModel>();
+            }
+        }
+    }
+}
