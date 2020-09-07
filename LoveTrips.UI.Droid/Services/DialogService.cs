7d220217de3d55414e906dd7e860ae5796c50cba
Ricardo Note <shyoutarou@gmail.com>
2020-09-07 18:00:35 -0300

    Add Dialog classes

---
+++ LoveTrips.UI.Droid/Services/DialogService.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using LoveTrips.Core.Interfaces.Services;
+using MvvmCross;
+using MvvmCross.Platforms.Android;
+
+namespace MyTrains.Droid.Services
+{
+    public class DialogService: IDialogService
+    {
+        protected Activity CurrentActivity => 
+            Mvx.IoCProvider.Resolve<IMvxAndroidCurrentTopActivity>().Activity;
+
+        public Task ShowAlertAsync(string message, 
+            string title, string buttonText)
+        {
+            return Task.Run(() =>
+            {
+                Alert(message, title, buttonText);
+            });
+        }
+
+        private void Alert(string message, string title, string okButton)
+        {
+            Application.SynchronizationContext.Post(ignored =>
+            {
+                var builder = new AlertDialog.Builder(CurrentActivity);
+                builder.SetIconAttribute
+                    (Android.Resource.Attribute.AlertDialogIcon);
+                builder.SetTitle(title);
+                builder.SetMessage(message);
+                builder.SetPositiveButton(okButton, delegate { });
+                builder.Create().Show();
+            }, null);
+        }
+    }
+}
