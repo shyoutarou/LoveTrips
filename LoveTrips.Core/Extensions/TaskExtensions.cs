50488ce94dc2e567a9d5e5f3abe3d5cbbc59fa9c
Ricardo Note <shyoutarou@gmail.com>
2020-09-07 17:56:16 -0300

    Add Extensions folder

---
+++ LoveTrips.Core/Extensions/TaskExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace LoveTrips.Core.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static void Forget(this Task task)
+        {
+            task.ConfigureAwait(false);
+        }
+    }
+}
